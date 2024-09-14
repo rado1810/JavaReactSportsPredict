d948102aaf7707476aa4fc332fc566aeef1bac5c
Radoslav <rado1810@yahoo.com>
2024-09-14 07:34:13 +0300

    remove unnecessary paths

--- Java-backend/src/main/java/bg/web/radoweb/service/Impl/CustomUserDetailsService.java
+++ Java-backend/src/main/java/bg/web/radoweb/service/Impl/CustomUserDetailsService.java
@@ -16,7 +16,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 
-import java.security.Key;
 import java.util.Set;
 import java.util.stream.Collectors;
 
